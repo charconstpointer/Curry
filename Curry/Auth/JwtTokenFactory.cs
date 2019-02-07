3a2ee9a9c168b3bff9e574dd714d3772d9267b7c
matkateresa <attwistingnether@gmail.com>
2019-02-07 11:16:30 +0100

    cleaned

--- Curry/Auth/JwtTokenFactory.cs
+++ Curry/Auth/JwtTokenFactory.cs
@@ -11,27 +11,29 @@ namespace Curry.Auth
     public class JwtTokenFactory : ITokenFactory<JwtSecurityToken>
     {
         private readonly IConfiguration _config;
+
         public JwtTokenFactory(IConfiguration config)
         {
             this._config = config;
         }
+
         public JwtSecurityToken GenerateToken(User user)
         {
             var claims = new[]
-                   {
-                        new Claim(JwtRegisteredClaimNames.Sub, user.Name),
-                        new Claim(JwtRegisteredClaimNames.Jti, user.Id.ToString()),
-                        new Claim(ClaimTypes.Role, "User")
-                    };
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Name),
+                new Claim(JwtRegisteredClaimNames.Jti, user.Id.ToString()),
+                new Claim(ClaimTypes.Role, "User")
+            };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
             var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(_config["Tokens:Issuer"],
-            _config["Tokens:Audience"],
-            claims,
-            expires: DateTime.Now.AddMinutes(1),
-            signingCredentials: credentials);
+                _config["Tokens:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(1),
+                signingCredentials: credentials);
             return token;
         }
     }
